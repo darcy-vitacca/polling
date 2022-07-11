fcdc53c3b7d075fc028ea5558de757317e6792c8
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2022-07-11 06:06:27 +0100

    updated styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,23 +9,27 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="p-6 flex flex-col">
-      <div className="flex flex-col">
-        <h1 className="font-bold text-2xl">Your Questions</h1>
-        {data?.map((item) => (
-          <div className="flex flex-col my-2" key={item.id}>
-            <Link href={`/question/${item.id}`}>
-              <div key={item.id} className="mb-4">
-                {item?.question}
+    <div className="p-6 flex flex-col w-screen items-stretch">
+      <div className="header flex w-full justify-between">
+        <div className="flex flex-col">
+          <h1 className="font-bold text-2xl">Your Questions</h1>
+          {data?.map((item) => (
+            <Link href={`/question/${item.id}`} key={item.id}>
+              <div className="flex flex-col my-2">
+                <div key={item.id} className="mb-4">
+                  {item?.question}
+                </div>
+                <span>Created on {item?.createdAt.toDateString()}</span>
               </div>
             </Link>
-            <span>Created on {item?.createdAt.toDateString()}</span>
-          </div>
-        ))}
+          ))}
+        </div>
+        <Link href="/create">
+          <a className="bg-gray-300 rounded text-gray-800 p-4 h-[60px]">
+            Create new question
+          </a>
+        </Link>
       </div>
-      <Link href="/create">
-        <a>Create new question</a>
-      </Link>
     </div>
   );
 };
